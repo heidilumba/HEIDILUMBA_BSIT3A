94feeaacb5a45f8b4c7e2d00a9756de458859a7c
charles lumba <clumba533@gmail.com>
2024-05-29 22:44:34 -0700

    commit

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -32,7 +32,7 @@
                                 
                             </div>
                             <div class="form-group">
-                                <button type="submit" class="btn btn-secondary btn-block">Register</button>
+                                <button type="submit" class="btn btn-primary btn-block">Register</button>
                             </div>
                         </form>
                     </div>
